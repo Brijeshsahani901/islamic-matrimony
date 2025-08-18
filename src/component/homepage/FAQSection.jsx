203d3e5dabb1e3f86ff2700d0f9f6c1c27e5f275
Ravish <ravish@LAPTOP-DHDMJQEM>
2025-08-18 17:24:42 +0530

    favicon updated

--- src/component/homepage/FAQSection.jsx
+++ src/component/homepage/FAQSection.jsx
@@ -36,13 +36,30 @@ const faqs = [
     question: "Can I talk directly to potential matches?",
     answer: "Communication is allowed but monitored or facilitated through guardians to maintain Islamic principles.",
   },
-  {
-    question: "What if I don’t have a guardian?",
-    answer: "If you don’t have a guardian, contact support for alternative arrangements.",
-  },
+ 
   {
     question: "What behavior is not allowed?",
     answer: "Disrespect, harassment, and inappropriate content are strictly prohibited and result in immediate suspension.",
+  },
+  {
+    question: "What happens if Islamic guidlines are violated?",
+    answer: "Profiles violating Islamic guidelines will be immediately suspended or permanently banned. We take Islamic compliance very seriously and have zero tolerance for inappropriate behavior.",
+  },
+  {
+    question: "Is there a refund policy?",
+    answer: "You are free to browse the site and understand our platform. Once payment is made, there are no refunds. This policy ensures commitment and prevents misuse, so please make sure you understand this before proceeding with your payment. If you have any questions, please contact us at help@marryingmuslims.com.",
+  },
+  {
+    question: "What payment methods are accepted?",
+    answer: "We accept PayPal, bank transfers, and all major credit and debit cards. Before we list your profile live on our site, you may be required to provide evidence of your email address, personal credentials, and bank history for confirmation purposes or in the event of a dispute. If you have questions, please reach out to us at help@marryingmuslims.com",
+  },
+   {
+    question: "How can i get help?",
+    answer: "For assistance, please contact help@marryingmuslims.com. We are here to help with profile setup, technical issues, payment problems and general platform questions. Our typical response time is 24–48 hours.",
+  },
+   {
+    question: "Online Nikah Service?",
+    answer: "We may be able to offer an online Nikah service, depending on availability. For more information, please email us at help@marryingmuslims.com.",
   },
 ];
 
